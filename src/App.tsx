92c6e5a922d8204d4f4525d2a3feded4f1b0ea22
Sophia Wagner <sophiewagnerla@gmail.com>
2022-07-25 13:47:07 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header> Breathwork Customizer</header>
+      <section>
+        <h1>APP TITLE</h1>
+        <p>Customizer goes here</p>
+      </section>
     </div>
   );
 }
